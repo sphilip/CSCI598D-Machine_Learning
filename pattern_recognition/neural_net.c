6d37d66a99d4b69b7c149107799d1bab8a74bf79
Shanley Philip@home <sphilip@mines.edu>
2011-10-08 16:32:18 -0600

    program doesn't work...need to test original again.  Started with backpropagation so saving this

--- pattern_recognition/neural_net.c
+++ pattern_recognition/neural_net.c
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
+#include <ctime>
+
 #include "stdio.h"
 #include "stdlib.h"
-#include <cstring>
 // #include "perceptron.h"
 
 using namespace std;
 
 // global variables
 int **alphabet;
-int *letters;
 int image_size;
+int alphabet_count;
 
-struct perceptron
+struct perceptron_layer
 {
   double weight;
   double value;
@@ -21,42 +23,46 @@ struct perceptron
 
 void read_input(const char* name, int index) // name of file & index in alphabet
 {
-  ifstream infile(name);
+  ifstream infile(name, ifstream::in);
   if (!infile)
   {
     cout << "Can't read from " << name << endl;
     return;
   }
 
-  char code[2];
-  int height;
-  int width;
-  int pixel1,pixel2,pixel3;
-
-  infile >> code >> width >> height;
-  image_size = width*height;
-
-  alphabet[index] = new int[width*height];
-
-  int i(0);
-  while (!infile.eof())
+  if (infile.is_open())
   {
-    pixel1 = -1;
-    pixel2 = -1;
-    pixel3 = -1;
-    infile >> pixel1 >> pixel2 >> pixel3;
-    
-    // scale pixel values b/w 0,1
-    if (pixel1 == 255)
-      pixel1 = 0;  // if white
-    
-    else pixel1 = 1; // if black
-      
-    alphabet[index][i] = pixel1;
-    i++;
+    char code[2];
+    int height;
+    int width;
+    int pixel1,pixel2,pixel3;
+
+    infile >> code;
+    infile >> width >> height;
+    image_size = width*height;
+
+    alphabet[index] = new int[width*height];
+
+    int i(0);
+    while (!infile.eof())
+    {
+      pixel1 = -1;
+      pixel2 = -1;
+      pixel3 = -1;
+      infile >> pixel1 >> pixel2 >> pixel3;
+
+      // scale pixel values b/w 0,1
+      if (pixel1 == 255)
+	pixel1 = 0;  // if white
+
+	else pixel1 = 1; // if black
+
+	  alphabet[index][i] = pixel1;
+	i++;
+    }
+
+    infile.close();
   }
-  
-  infile.close();
 }
 
 
@@ -69,7 +75,7 @@ bool valid_file(const char* name)
 }
 
 
-void test_alphabet()
+void test_alphabet(int alphabet_count)
 {
   for (int i=0; i<26; i++)
   {
@@ -80,7 +86,7 @@ void test_alphabet()
       else
         cout << alphabet[i][j] << " ";
     }
-    
+
     cout << "\n\n";
   }
 }
@@ -90,18 +96,81 @@ void clear_memory(int count)
 {
   for (int i=0;i<count;i++)
     delete[] alphabet[i];
-  
+
   delete[] alphabet;
 }
 
 
+void backpropagation(int input_size,int hidden_size, int output_size, double learning_rate)
+{
+  perceptron_layer* input = new perceptron_layer[input_size];
+  perceptron_layer* hidden = new perceptron_layer[hidden_size];
+  perceptron_layer* output = new perceptron_layer[output_size];
+
+  // initialize weights
+  for (int i=0; i<input_size; i++)
+  {
+    input[i].value = 0.0;
+    input[i].weight = 1.0;//((double)rand())/((double)RAND_MAX) -1.0f;
+
+    if (i<hidden_size)
+    {
+      hidden[i].value = 0.0;
+      hidden[i].weight = ((double)rand())/((double)RAND_MAX) - 1.0f;
+    }
+
+    if (i<output_size)
+    {
+      output[i].value = 0.0;
+      output[i].weight = ((double)rand())/((double)RAND_MAX) - 1.0f;
+    }
+  }
+
+
+  for (int k=0; k<alphabet_count; k++)
+  {
+    // initialize inputs
+    for (int j=0; j<input_size; j++)
+      input[j].value = alphabet[k][j];
+
+    // train with alphabet
+    for (int j=0; j<hidden_size; j++)
+    {
+      for (int i=0;i<input_size; i++)
+      {
+	hidden[j].value += hidden[j].weight * input[i].value;
+      }
+    }
+
+    // calculate output and backpropagate errors
+    for (int j=0; j<output_size; j++)
+    {
+      for (int i=0;i<hidden_size; i++)
+      {
+	output[j].value += output[j].weight * hidden[i].value;
+      }
+    }
+  }
+
+  for (int z=0;z<output_size; z++)
+  {
+    cout << (char)(z + 97) << "\t" << output[z].value << endl;
+  }
+
+  delete[] input;
+  delete[] hidden;
+  delete[] output;
+}
+
 int main()
 {
+  srand(time(NULL));
+
   string filename;
   int a = 97;
   int z = 122;
   char letter;
-  int valid_count = 0;
+  alphabet_count = 0;
 
   // how many valid files exist?
   for (int i=a; i<=z; i++)
@@ -109,10 +178,11 @@ int main()
     sprintf(&letter,"%c",i);
     filename = "./letters/" + string (1,letter) + ".ppm";
 
-    if (valid_file) valid_count++;
+    if ( valid_file(filename.c_str()) )
+      alphabet_count++;
   }
 
-  alphabet = new int*[valid_count];
+  alphabet = new int*[alphabet_count];
 
   // save pixel values for each valid file
   for (int i=a; i<=z; i++)
@@ -122,15 +192,15 @@ int main()
 
     read_input(filename.c_str(),(i-97));
   }
-  
+
   //   test_alphabet();
-  
-  
-  
-  
-  
-  
-  
-  clear_memory(valid_count);
+
+//   backpropagation(image_size,alphabet_count,alphabet_count,0.5);
+
+
+
+
+
+  clear_memory(alphabet_count);
   return 0;
 }
